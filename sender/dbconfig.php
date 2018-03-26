d3813c05cb537c0618faf4a3b67b732d05a5014d
Roman Zozulia <r@zozuliar.push-k>
2018-03-26 14:31:39 +0300

    init3

--- sender/dbconfig.php
+++ sender/dbconfig.php
@@ -1,12 +1,5 @@
 <?php
-//define('DBHost2','localhost');
-//define('DBUser2','tarolog');
-//define('DBPassword2','wXRarVZE');
-//define('DBName2','tarolog');
-//define('DBPort2',3306);
-//define('DBCharSet2', 'utf8');
 define('first_delay', 0);
 define('second_delay', 3);
 define('third_delay', 7);
-
 ?>
