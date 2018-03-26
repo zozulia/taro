7efa1e37e0e7ca80056ee61cc33e3868c49964ef
Roman Zozulia <r@zozuliar.push-k>
2018-03-26 14:40:24 +0300

    init4.0

--- get_adresat.php
+++ get_adresat.php
@@ -3,6 +3,9 @@
 require_once('./edit/lib_db.php');
 require_once('./sender/lib_mail.php');
 
+define('ownermail','korchenkosv@gmail.com');
+//define('ownermail','roman.zozulia@gmail.com');
+
 $db = new DBMySQLPDO();
 $sql = 'SELECT text FROM polia WHERE block_name=\'vebinar\' AND block_order=1 AND field_name=\'date\';';
 $db->execute($sql);
@@ -23,17 +26,20 @@ $headers .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 //send('korchenkosv@gmail.com', $name, $msg);
 mail('korchenkosv@gmail.com, roman.zozulia@gmail.com', 'Novaya zayavka', $msg, $headers);
 
-$sql = 'SELECT signed FROM adresat WHERE email=?;';
-$db->execute($sql,true,array($email));
-if (is_array($db->_rows) && array_key_exists(0, $db->_rows) && array_key_exists('signed', $db->_rows[0])){
-	die('You are allready in our database');
+if ($email != ownermail){
+	$sql = 'SELECT signed FROM adresat WHERE email=?;';
+	$db->execute($sql,true,array($email));
+	if (is_array($db->_rows) && array_key_exists(0, $db->_rows) && array_key_exists('signed', $db->_rows[0])){
+		die('You are allready in our database');
+	}
 }
-
 $name = addslashes($_POST['name']);
 $phone = (array_key_exists('phone',$_POST))?$_POST['phone']:'0';
 $sql='INSERT INTO adresat(email, name, phone, signed, reg_date, first_date) VALUES(?,?,?,0,CURDATE(),CURDATE())';
 $params = array($email,$name,$phone);
-$db->execute($sql, false, $params);
+if ($email != ownermail){
+	$db->execute($sql, false, $params);
+}
 
 $template = file_get_contents('sender/letter0.html');
 $template = strtr($template,array('{--date--}'=>$date));
@@ -42,6 +48,16 @@ send($email, '', $template);
 $template = file_get_contents('sender/letter1.html');
 $template = strtr($template,array('{--date--}'=>$date));
 send($email, $name, $template);
+if ($email == ownermail){
 
+	$template = file_get_contents('./sender/letter2.html');
+	$template = strtr($template,array('{--date--}'=>$date));
+	send($email, $_POST['name'], $template);
+
+	$template = file_get_contents('./sender/letter3.html');
+	$template = strtr($template,array('{--date--}'=>$date));
+	send($email, $_POST['name'], $template);
+
+}
 //header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
--- sender/dbconfig.php
+++ sender/dbconfig.php
@@ -1,5 +1,12 @@
 <?php
+//define('DBHost2','localhost');
+//define('DBUser2','tarolog');
+//define('DBPassword2','wXRarVZE');
+//define('DBName2','tarolog');
+//define('DBPort2',3306);
+//define('DBCharSet2', 'utf8');
 define('first_delay', 0);
 define('second_delay', 3);
 define('third_delay', 7);
+
 ?>
